5413acd83ef6431d5fe77e2d2407105ac838dd7e
Mathieu Ripert <mathieu.ripert@gmail.com>
2025-10-02 09:29:52 +0200

    Update markdown_splitter.rb

--- lib/baran/markdown_splitter.rb
+++ lib/baran/markdown_splitter.rb
@@ -2,21 +2,28 @@ require_relative './recursive_character_text_splitter'
 
 module Baran
   class MarkdownSplitter < RecursiveCharacterTextSplitter
+
+    SEPARATORS = [
+      /\n# /, # h1
+      /\n## /, # h2
+      /\n### /, # h3
+      /\n#### /, # h4
+      /\n##### /, # h5
+      /\n###### /, # h6
+      /```\n\n/, # code block
+      /\n\n\*{3,}\n\n/, # horizontal rule (***)
+      /\n\n-{3,}\n\n/, # horizontal rule (---)
+      /\n\n_{3,}\n\n/, # horizontal rule (___)
+      "\n\n", # paragraph break
+    ]
+    
     def initialize(chunk_size: 1024, chunk_overlap: 64, token_count_fn: nil)
-      separators = [
-        /\n# /, # h1
-        /\n## /, # h2
-        /\n### /, # h3
-        /\n#### /, # h4
-        /\n##### /, # h5
-        /\n###### /, # h6
-        /```\n\n/, # code block
-        /\n\n\*{3,}\n\n/, # horizontal rule (***)
-        /\n\n-{3,}\n\n/, # horizontal rule (---)
-        /\n\n_{3,}\n\n/, # horizontal rule (___)
-        "\n\n", # paragraph break
-      ]
-      super(chunk_size: chunk_size, chunk_overlap: chunk_overlap, separators: separators, token_count_fn: token_count_fn)
+      super(
+        chunk_size: chunk_size,
+        chunk_overlap: chunk_overlap,
+        separators: SEPARATORS,
+        token_count_fn: token_count_fn
+      )
     end
   end
 end
